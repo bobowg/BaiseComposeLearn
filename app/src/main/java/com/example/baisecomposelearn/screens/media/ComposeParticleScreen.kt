d091cb46662e004c39b0c720c2fcce6908ed03d2
bobowg <bobowg@163.com>
2022-09-03 15:01:22 +0800

    sixteen commit

--- app/src/main/java/com/example/baisecomposelearn/screens/media/ComposeParticleScreen.kt
+++ app/src/main/java/com/example/baisecomposelearn/screens/media/ComposeParticleScreen.kt
@@ -1,14 +1,26 @@
 package com.example.baisecomposelearn.screens.media
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.baisecomposelearn.screens.components.SnowFall
+import com.example.baisecomposelearn.screens.components.*
 
 @Composable
 fun ComposeParticleScreen(navController: NavController) {
-    SnowFall()
+    val num = (1..5).random()
+    Surface {
+        when (num) {
+            1 -> Fountain()
+            2 -> Confetti()
+            3 -> Meteor()
+            4 -> Explosion()
+            5 ->  SnowFall()
+        }
+
+    }
+
 }
 
 @Preview

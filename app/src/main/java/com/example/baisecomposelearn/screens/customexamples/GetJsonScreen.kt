dad0ee407790df9bcb41f9cb4a0e661b17b8b48f
bobowg <bobowg@163.com>
2022-09-28 20:27:01 +0800

    thridty one by bobowg

--- app/src/main/java/com/example/baisecomposelearn/screens/customexamples/GetJsonScreen.kt
+++ app/src/main/java/com/example/baisecomposelearn/screens/customexamples/GetJsonScreen.kt
@@ -1,6 +1,6 @@
 package com.example.baisecomposelearn.screens.customexamples
 
-import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -20,7 +20,6 @@ import androidx.navigation.compose.rememberNavController
 import com.example.baisecomposelearn.screens.components.ScreenModel
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
-import kotlinx.coroutines.DelicateCoroutinesApi
 import org.json.JSONObject
 import org.json.JSONTokener
 import java.net.URL
@@ -28,31 +27,107 @@ import javax.net.ssl.HttpsURLConnection
 
 @Composable
 fun GetJsonScreen(navController: NavController) {
-    var isopen by remember { mutableStateOf(false) }
+    var issimple by remember { mutableStateOf(false) }
+    var isnestedJSON by remember { mutableStateOf(false) }
+    var isarrayJSON by remember { mutableStateOf(false) }
     ScreenModel(navController = navController, content = {
-        if (isopen) {
+        if (issimple) {
             JsonCard()
+        }
+        if (isnestedJSON) {
+            nestedCard()
         }
         Button(
             onClick = {
-                isopen = true
-
-            },
-            modifier = Modifier
+                issimple = true
+                isnestedJSON = false
+                isarrayJSON = false
+            }, modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 16.dp, end = 16.dp)
         ) {
             Text(text = "简单JSON示例", fontSize = 24.sp)
         }
+        Button(
+            onClick = {
+                issimple = false
+                isnestedJSON = true
+                isarrayJSON = false
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Text(text = "非索引JSON示例", fontSize = 24.sp)
+        }
+        Button(
+            onClick = {
+                issimple = false
+                isnestedJSON = false
+                isarrayJSON = true
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Text(text = "组数JSON示例", fontSize = 24.sp)
+        }
     })
 }
 
-@SuppressLint("CoroutineCreationDuringComposition")
-@OptIn(DelicateCoroutinesApi::class)
+@Composable
+fun nestedCard() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        val url = URL("https://bobowg.pythonanywhere.com/static/img/nested.json")
+        val httpsURLConnection = url.openConnection() as HttpsURLConnection
+        httpsURLConnection.setRequestProperty("Accept", "application/json")
+        httpsURLConnection.requestMethod = "GET"
+        httpsURLConnection.doInput = true
+        httpsURLConnection.doOutput = false
+        val responseCode = httpsURLConnection.responseCode
+        if (responseCode == HttpsURLConnection.HTTP_OK) {
+            val response = httpsURLConnection.inputStream.bufferedReader().use { it.readText() }
+            val gson = GsonBuilder().setPrettyPrinting().create()
+            val prettyJson = gson.toJson(JsonParser.parseString(response))
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.Center)) {
+                Text(
+                    text = prettyJson,
+                    modifier = Modifier
+                        .background(color = Color.White),
+                    textAlign = TextAlign.Center,
+                    color = Color.Black
+                )
+                val jsonObject = JSONTokener(response).nextValue() as JSONObject
+                val jsonArray = jsonObject.getJSONArray("data")
+                for (i in 0 until jsonArray.length()) {
+                    Text(text = jsonArray.getJSONObject(i).getString("id") )
+                    val employee = jsonArray.getJSONObject(i).getJSONObject("employee")
+                    Text(text = employee.toString())
+                    Text(text = employee.getString("name") )
+                    val employeeSalary = employee.getJSONObject("salary")
+                    val employeeSalaryUSD = employeeSalary.getInt("usd")
+                    Text(text = employeeSalaryUSD.toString())
+                    val employeeSalaryEUR = employeeSalary.getInt("eur")
+                    Text(text = employeeSalaryEUR.toString())
+                    val employeeAge = employee.getString("age")
+                    Text(text = employeeAge)
+                }
+            }
+        }else{
+            Log.e("HTTPURLCONNECTION_ERROR", responseCode.toString())
+        }
+    }
+}
+
 @Composable
 fun JsonCard() {
     Box(modifier = Modifier.fillMaxSize()) {
-        Column (modifier = Modifier.fillMaxWidth().padding(16.dp)){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
             val url = URL("https://bobowg.pythonanywhere.com/static/img/simplae.json")
             val httpsURLConnection = url.openConnection() as HttpsURLConnection
             httpsURLConnection.setRequestProperty("Accept", "application/json")
@@ -66,7 +141,8 @@ fun JsonCard() {
                 val gson = GsonBuilder().setPrettyPrinting().create()
                 val prettyJson = gson.toJson(JsonParser.parseString(response))
                 Text(
-                    text = prettyJson, modifier = Modifier
+                    text = prettyJson,
+                    modifier = Modifier
                         .background(color = Color.White)
                         .align(
                             Alignment.CenterHorizontally

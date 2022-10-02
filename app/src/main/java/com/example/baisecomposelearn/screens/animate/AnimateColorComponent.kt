d60b522fc35fb9bea2d8b62bd374c339ede0dd3e
bobowg <bobowg@163.com>
2022-10-02 16:17:40 +0800

    thirdty three by bobowg

--- app/src/main/java/com/example/baisecomposelearn/screens/animate/AnimateColorComponent.kt
+++ app/src/main/java/com/example/baisecomposelearn/screens/animate/AnimateColorComponent.kt
@@ -1,36 +1,46 @@
 package com.example.baisecomposelearn.screens.animate
 
 import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
+enum class ColorSate {
+    Black,
+    Red,
+    Green,
+    Blue;
+}
+
 @Composable
 fun AnimateColorComponent() {
-    val currentColor by remember { mutableStateOf(Color.Red) }
-    val transition = updateTransition(currentColor, label = "color")
-    val color by transition.animateColor(
-        transitionSpec = { TweenSpec<Color>(durationMillis = 2000) }, label = "animatecolor"
-    ) { state ->
-        when (state) {
-            Color.Red -> Color.Green
-            Color.Green -> Color.Blue
-            Color.Blue -> Color.Red
-            else -> Color.Red
-        }
+    var state by remember { mutableStateOf(ColorSate.values().random()) }
+    val color = when(state){
+        ColorSate.Blue -> Color.Blue
+        ColorSate.Red -> Color.Red
+        ColorSate.Green -> Color.Green
+        ColorSate.Black -> Color.Black
     }
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(color = color)) { }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable {
+                state = ColorSate
+                    .values()
+                    .random()
+            }
+            .background(color = color)
+    ) {
+
+    }
 
 }
 

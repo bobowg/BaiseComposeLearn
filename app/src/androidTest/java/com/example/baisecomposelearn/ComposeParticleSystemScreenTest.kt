5893aa8b764b469ad1ffe33cd094bb0b377b2825
bobowg <bobowg@163.com>
2022-10-03 12:03:15 +0800

    thridty four by bobowg

--- app/src/androidTest/java/com/example/baisecomposelearn/ComposeParticleSystemScreenTest.kt
+++ app/src/androidTest/java/com/example/baisecomposelearn/ComposeParticleSystemScreenTest.kt
@@ -2,11 +2,15 @@ package com.example.baisecomposelearn
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.compose.rememberNavController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.baisecomposelearn.screens.animate.SingleValueFloatAnimationScreen
 import com.example.baisecomposelearn.screens.media.ComposeParticleScreen
 import com.example.baisecomposelearn.theme.BaiseComposeLearnTheme
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class ComposeParticleSystemScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -20,5 +24,13 @@ class ComposeParticleSystemScreenTest {
             }
         }
         composeTestRule.mainClock.advanceTimeBy(10 * 1000)
+    }
+    @Test
+    fun testSingleValueFloatAnimationScreen(){
+        composeTestRule.setContent {
+            BaiseComposeLearnTheme {
+                SingleValueFloatAnimationScreen(rememberNavController())
+            }
+        }
     }
 }

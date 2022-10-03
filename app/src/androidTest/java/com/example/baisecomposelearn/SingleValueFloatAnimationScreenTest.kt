9fb1d96808d9159f3c33a4aec71ac59e9d550146
bobowg <bobowg@163.com>
2022-10-04 01:07:48 +0800

    thidty four by bobowg

--- app/src/androidTest/java/com/example/baisecomposelearn/SingleValueFloatAnimationScreenTest.kt
+++ app/src/androidTest/java/com/example/baisecomposelearn/SingleValueFloatAnimationScreenTest.kt
@@ -4,32 +4,21 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.baisecomposelearn.screens.animate.SingleValueFloatAnimationScreen
-import com.example.baisecomposelearn.screens.media.ComposeParticleScreen
 import com.example.baisecomposelearn.theme.BaiseComposeLearnTheme
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ComposeParticleSystemScreenTest {
+class SingleValueFloatAnimationScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
-    fun testComposeParticleSystemScreen(){
-        composeTestRule.mainClock.autoAdvance = false
+    fun testSingleValueFloatAnimationScreen() {
         composeTestRule.setContent {
             BaiseComposeLearnTheme {
-                ComposeParticleScreen(rememberNavController())
-            }
-        }
-        composeTestRule.mainClock.advanceTimeBy(10 * 1000)
-    }
-    @Test
-    fun testSingleValueFloatAnimationScreen(){
-        composeTestRule.setContent {
-            BaiseComposeLearnTheme {
-                SingleValueFloatAnimationScreen(rememberNavController())
+                SingleValueFloatAnimationScreen(navController = rememberNavController())
             }
         }
     }

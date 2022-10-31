a308fed8dbc7a924db08d99a2f827c7d5ca5d694
bobowg <bobowg@163.com>
2022-10-31 13:32:45 +0800

    fifty-one by bobowg

--- app/src/main/java/com/example/baisecomposelearn/weather/domain/location/LocationTracker.kt
+++
@@ -1,7 +0,0 @@
-package com.example.baisecomposelearn.weather.domain.location
-
-import android.location.Location
-
-interface LocationTracker {
-    suspend fun getCurrentLocation():Location?
-}
--- app/src/main/java/com/example/baisecomposelearn/weather/domain/repository/WeatherRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.baisecomposelearn.weather.domain.repository
-
-import com.bumptech.glide.load.engine.Resource
-import com.example.baisecomposelearn.weather.domain.info.WeatherInfo
-
-interface WeatherRepository {
-    suspend fun getWeatherData(lat:Double,long:Double): com.example.baisecomposelearn.weather.domain.util.Resource<WeatherInfo>
-}

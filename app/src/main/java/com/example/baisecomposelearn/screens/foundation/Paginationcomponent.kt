1547ef9319f511f7a329d27548875adedb18b3ee
bobowg <bobowg@163.com>
2023-02-01 10:19:49 +0800

    优化了一些不必要的组件。

--- app/src/main/java/com/example/baisecomposelearn/screens/foundation/Paginationcomponent.kt
+++ app/src/main/java/com/example/baisecomposelearn/screens/foundation/Paginationcomponent.kt
@@ -1,16 +1,16 @@
 package com.example.baisecomposelearn.screens.foundation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.baisecomposelearn.screens.components.goBack
 import com.example.baisecomposelearn.screens.foundation.data.PaginatorViewModell
 import com.example.baisecomposelearn.theme.best
@@ -20,17 +20,40 @@ import com.example.baisecomposelearn.theme.darkTextColorPrimary
 fun Paginationcomponent(navController: NavController) {
     val viewmodel = viewModel<PaginatorViewModell>()
     val state = viewmodel.state
-    LazyColumn(modifier = Modifier.fillMaxSize()){
-        items(state.items.size){ i ->
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(state.items.size) { i ->
             val item = state.items[i]
-            Column {
+            if (i >= state.items.size - 1 && !state.endReached && !state.isLoading) {
+                viewmodel.loadNextItems()
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
                 Text(text = item.title, fontFamily = best, color = darkTextColorPrimary)
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(text = item.description, fontFamily = best, color = darkTextColorPrimary)
             }
         }
         item {
+            if (state.isLoading) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
             goBack(navController = navController)
         }
     }
+}
+
+@Preview
+@Composable
+fun PaginatorPreview() {
+    Paginationcomponent(navController = rememberNavController())
 }
